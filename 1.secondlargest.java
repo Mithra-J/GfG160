abf5b49aeabb6a348d5539a877a1c33f6af54a0a
Mithra J <mithraacct@gmail.com>
2024-11-16 22:51:59 +0530

    Update 1.secondlargest.java

--- 1.secondlargest.java
+++ 1.secondlargest.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int lar=arr[0];
+        int sec=-1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>lar){
+                sec=lar;
+                lar=arr[i];
+            }
+            else if(arr[i]<lar&&arr[i]>sec){
+                sec=arr[i];
+            }
+        }
+        return sec;
+    }
+}
